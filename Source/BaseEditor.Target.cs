f947db091b36a4580127f8e58dbfdc63854ade97
Przemysław Kurowski <pkurowski@incuvo.com>
2025-12-12 15:51:57 +0100

    Migration to 5.4.

--- Source/BaseEditor.Target.cs
+++ Source/BaseEditor.Target.cs
@@ -8,8 +8,8 @@ public class BaseEditorTarget : TargetRules
 	public BaseEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("Base");
 	}
 }
